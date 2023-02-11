13395fdbb2e206ee5ee025db2a8c7831eac0a1d0
LOVKUSH <lovkushyadav9888@gmail.com>
2023-02-12 04:50:52 +0530

    Char Array

---
+++ Array/characterArray/intro.cpp
@@ -0,0 +1,13 @@
+//We donot need for loop for taking input from the user
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char name[10];
+    cout<<"Enter Your Name Here"<<endl;
+
+    cin>>name;
+    cout<<name<<endl;
+    return 0;
+}
