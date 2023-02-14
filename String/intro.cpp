41a7a40847523ee023e5471d0ea61e430c4eeee1
LOVKUSH <lovkushyadav9888@gmail.com>
2023-02-14 07:52:05 +0530

    Properties of string

--- String/intro.cpp
+++ String/intro.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include<vector>
+#include<algorithm>
 using namespace std;
 
 int main()
@@ -24,8 +25,31 @@ int main()
     ///Printing this By iteration = for loop
     for(int i=0; i<v.size(); i++){
         cout<<v[i]<<endl;
+        ///We can also sort this string by:
+        sort(v.begin(), v.end());
+        cout<<v[i]<<endl;
     }
 
+
+    //Another way === by taking input from the user
+    string s3;
+    /*cin>>s3;*/   //dont use this 
+    getline(cin, s3);  //This will also consider the spaces in between them //This is the method in string for taking input -Here cin.getline doesnt work
+    cout<<s3<<endl;
+
+
+    ///We can aslo cange the string by:
+    s3 = "abcabc"
+    cout<<s3[1]<<endl;
+
+
+
+    //Adding of two string
+    string s4 = s + s3;
+
+    ///also we can add the string
+     string s4 = s + s3 + "lovkush Yadav";
+    cout<<s4<<endl;
 
     return 0;
 }
