264fa7601bf60886f6732f437c6df62b8175302d
LOVKUSH <lovkushyadav9888@gmail.com>
2023-02-14 06:12:02 +0530

    Passing array in func

--- Array/2D-Array(Matrix)/printElemtof2-D-array.cpp
+++ Array/2D-Array(Matrix)/printElemtof2-D-array.cpp
@@ -16,3 +16,37 @@ int main()
     
     return 0;
 }
+
+
+//Giving the value of column is mandatory
+
+//Passing the value in a function
+
+void func(int m, int n, int arr[][n])       //function prototype
+
+// The following program works only if your compiler is C99 compatible.
+
+#include <iostream>
+using namespace std;
+
+// n must be passed before the 2D array
+
+void print(int m, int n, int arr[][n])
+{
+	int i, j;
+	for (i = 0; i < m; i++)
+	for (j = 0; j < n; j++)
+		printf("%d ", arr[i][j]);
+}
+
+int main()
+{
+	int arr[][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+	int m = 3, n = 3;
+	print(m, n, arr);
+	return 0;
+}
+
+
+
+
