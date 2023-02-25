b0c36c5da94814f84e5e428b6ed8bf6afe169132
LOVKUSH <lovkushyadav9888@gmail.com>
2023-02-25 18:41:38 +0530

    Linked_List Basics

---
+++ Linked_List/intro.cpp
@@ -0,0 +1,6 @@
+/// elements are not stored at contiguous memory locations
+///elements in a linked list are linked using pointers
+//It has NODE+NEXT
+
+//WE use POinter = HEAD == its shows where the linked list starts
+//NULL = It shows thats its Last Node
