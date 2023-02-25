f253632f2f317ba455a3b8cb8f77d46fec2468dc
LOVKUSH <lovkushyadav9888@gmail.com>
2023-02-25 19:30:13 +0530

    Access Modifiers

--- OOPS/intro.cpp
+++ OOPS/intro.cpp
@@ -1,19 +1,23 @@
-//OOPS Goal =  aims to implement real-world entities like inheritance, hiding, polymorphism, etc in programming. 
-//OOP is to bind together the data and the functions
+// OOPS Goal =  aims to implement real-world entities like inheritance, hiding, polymorphism, etc in programming.
+// OOP is to bind together the data and the functions
 
+1. CLASS = Main template == BluePrint == = user defined dataType 2. Object = Copied version of class == make the copy & filled data == Real World
 
-1. CLASS = Main template == BluePrint === user defined dataType
-2. Object = Copied version of class == make the copy & filled data == Real World
-
-//Creating a class :
+// Creating a class :
 
 #include <iostream>
-using namespace std;
+                                                                                                                            using namespace std;
 
-//Syntax of creating a class
+// Syntax of creating a class
 
-class Student {
-    int rollNumber;   
+class Student
+{
+    //Making rollNumber - Public due to access modifiers
+    public :
+    
+    int rollNumber;
+
+    //Making age - Private due to access modifiers
     int age;
 };
 
@@ -21,21 +25,30 @@ class Student {
 
 int main()
 {
-    //Creating objetct staically
+    // Creating objetct staically
     Student s1;
     Student s2;
     Student s3;
 
+    // Creating objetct Dynamically
+    Student *s6 = new Student; //* = Derefence variable
 
-    //Creating objetct Dynamically
-    Student * s6 = new Student;
-    
-    //Accessing properties of elements ==by using .(dot)
+    // Accessing properties of elements ==by using .(dot)  == Statically
     s1.age = 24;
     s1.rollNumber = 100;
 
-    cout<<s1.age<<endl;
+    cout << s1.age << endl;
 
-    
+    // Acccessing poperties Dynamically
+    (*s6).age = 23;
+    (s6->rollNumber) = 30;
+
+    // Another way of this
+    s6->age = 23;
+    s6->rollNumber = 100;
+
     return 0;
 }
+
+/// OOPS has --- Access Modifiers
+// 3. Types = PUBLIC, PRIVATE & PROTECTED
