1a9fc3e0b3a6e379da277fc64cf7da5e491d62f7
LOVKUSH <lovkushyadav9888@gmail.com>
2023-02-25 18:54:50 +0530

    OOPS

--- Linked_List/intro.cpp
+++ Linked_List/intro.cpp
@@ -4,3 +4,9 @@
 
 //WE use POinter = HEAD == its shows where the linked list starts
 //NULL = It shows thats its Last Node
+
+// Structure of the node of a singly linked list
+typedef struct node{   //typedef-struct = Class
+    int data;
+    node* next;
+}node;
---
+++ OOPS/intero.cpp
@@ -0,0 +1,2 @@
+//OOPS Goal =  aims to implement real-world entities like inheritance, hiding, polymorphism, etc in programming. 
+//OOP is to bind together the data and the functions
