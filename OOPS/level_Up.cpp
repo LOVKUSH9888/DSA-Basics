7d6a8b839feab51af6d82d9fca8d3b192d8ffa5c
LOVKUSH <lovkushyadav9888@gmail.com>
2023-02-27 21:30:03 +0530

    Further in consturctor

--- OOPS/level_Up.cpp
+++ OOPS/level_Up.cpp
@@ -29,28 +29,31 @@ int main()
 }
 
 /// Constructures =  special method that is automatically called when an object of a class is created.
-//It is used to initialize the data members of new objects
-class Myclass
+// It is used to initialize the data members of new objects == For Garbase Values
+// 1. Same name as class name
+// 2.No return type
+// 3.No input argument
+
+/*class Myclass
 {
 public:
     Myclass() // Constructor
-    { 
+    {
         cout << "Lovkush";
     }
-}
-
-
+}*/
 
 // defining the constructor within the class
-  
+
 #include <iostream>
 using namespace std;
-  
-class student {
+
+class student
+{
     int rno;
     char name[10];
     double fee;
-  
+
 public:
     student()
     {
@@ -61,18 +64,22 @@ public:
         cout << "Enter the Fee:";
         cin >> fee;
     }
-  
+
     void display()
     {
-        cout << endl << rno << "\t" << name << "\t" << fee;
+        cout << endl
+             << rno << "\t" << name << "\t" << fee;
     }
 };
-  
+
 int main()
 {
     student s; // constructor gets called automatically when
                // we create the object of the class
     s.display();
-  
+
     return 0;
 }
+
+// We get the default when we create the class So no need to create the new constructor
+
