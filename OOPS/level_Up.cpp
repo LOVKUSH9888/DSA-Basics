dc437cf124579b65a142e2d015931df5c9fc962b
LOVKUSH <lovkushyadav9888@gmail.com>
2023-02-27 20:56:58 +0530

    Constructor in C++

--- OOPS/level_Up.cpp
+++ OOPS/level_Up.cpp
@@ -28,10 +28,51 @@ int main()
     return 0;
 }
 
+/// Constructures =  special method that is automatically called when an object of a class is created.
+//It is used to initialize the data members of new objects
+class Myclass
+{
+public:
+    Myclass() // Constructor
+    { 
+        cout << "Lovkush";
+    }
+}
 
 
 
-
-
-
-
+// defining the constructor within the class
+  
+#include <iostream>
+using namespace std;
+  
+class student {
+    int rno;
+    char name[10];
+    double fee;
+  
+public:
+    student()
+    {
+        cout << "Enter the RollNo:";
+        cin >> rno;
+        cout << "Enter the Name:";
+        cin >> name;
+        cout << "Enter the Fee:";
+        cin >> fee;
+    }
+  
+    void display()
+    {
+        cout << endl << rno << "\t" << name << "\t" << fee;
+    }
+};
+  
+int main()
+{
+    student s; // constructor gets called automatically when
+               // we create the object of the class
+    s.display();
+  
+    return 0;
+}
