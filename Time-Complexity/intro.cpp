813e047980a4a7bc0ba75088c0965575e818d0a1
LOVKUSH <lovkushyadav9888@gmail.com>
2023-03-01 07:40:59 +0530

    Big-O notation

--- Time-Complexity/intro.cpp
+++ Time-Complexity/intro.cpp
@@ -1,2 +1,10 @@
 //Mainly 2 type of analysis = Experimental Analysis
 //2.Theortical Analysis
+
+
+///Big-O Notation
+//1. We dont case the coefficient
+//2.We only focus on Higest Power
+
+
+///We will always find our time complexity for Worst Case
