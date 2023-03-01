8918134f6219f34e8057151c8d010aabacde0334
LOVKUSH <lovkushyadav9888@gmail.com>
2023-03-01 07:45:08 +0530

    # three cases of Time Complexity

--- Time-Complexity/intro.cpp
+++ Time-Complexity/intro.cpp
@@ -1,10 +1,16 @@
-//Mainly 2 type of analysis = Experimental Analysis
-//2.Theortical Analysis
+// Mainly 2 type of analysis = Experimental Analysis
+// 2.Theortical Analysis
 
+/// 3(Three) Cases :
 
-///Big-O Notation
-//1. We dont case the coefficient
-//2.We only focus on Higest Power
+// 1. Best Case  == Element is present at 1st Index
 
+// 2. Average CAse
 
-///We will always find our time complexity for Worst Case
+// 3.Worst CAse = Element is not present in Array
+
+/// Big-O Notation
+// 1. We dont case the coefficient
+// 2.We only focus on Higest Power
+
+/// We will always find our time complexity for Worst Case
