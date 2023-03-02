2c414c9a9b895f0941005eb25b4f1ef6ae83a927
LOVKUSH <lovkushyadav9888@gmail.com>
2023-03-02 21:57:29 +0530

    Pointers in C++

--- Array/Two-Pointer/intro.cpp
+++ Array/Two-Pointer/intro.cpp
@@ -1,3 +1,36 @@
+//Pointer in c++ :-)
+///Pointer = Pointers are symbolic representations of addresses
+//In memories - Symbol Table is created that basically stores the value in Bytes
+///Use = Iterate over Array
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+    int i;
+    cout <<&i<<endl;     //Ampercent of i == Address of i
+
+    //lets create a Pointer now for storing the above I value 
+    int * p = &i;
+    cout<<p<<endl;
+
+    //Printing the again i value = Dereference Operator  == *p
+    cout << *p << endl;
+
+    //Now if I increase the value of i
+    i++; //it means increment i = 1 + 10 = 11;
+
+    ///By doint the above case the Pointer value will also increase
+    return 0;
+}
+
+
+
+
+
+
 // Used for searching pairs in a sorted array.
 /*used to solve problems that involve searching, sorting, or manipulating arrays, strings, or linked lists. This approach involves using two pointers (indices) to traverse an array or list in a specific way to solve the problem.
 
@@ -16,3 +49,22 @@ Here are some notes for beginners in C++ who want to learn the two pointer appro
 6.Check the termination condition: Once the traversal is complete, check the termination condition to ensure that the two pointers have not crossed over each other. If they have, then you have processed all the elements.
 
 7.Debugging: Debugging is an important step to ensure that your two pointer approach is correct. Use print statements or debuggers to check the values of the pointers and the updated solution at each step of the traversal.*/
+
+
+//Example of sliding wimdow:-
+int left = 0, right = 0;
+int sum = 0;
+int ans = INT_MAX;
+while (right < n) {
+    sum += arr[right];
+    while (sum >= target) {
+        ans = min(ans, right - left + 1);
+        sum -= arr[left];
+        left++;
+    }
+    right++;
+}
+if (ans == INT_MAX) {
+    ans = 0;
+}
+return ans;
