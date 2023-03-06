94780bdc6788c60bf29801348209da08be5b002f
LOVKUSH <lovkushyadav9888@gmail.com>
2023-03-06 22:20:33 +0530

    STACK-Using Linked List

--- STACK/intro.cpp
+++ STACK/intro.cpp
@@ -82,3 +82,49 @@ int main()
 
     return 0;
 }
+
+//STACK-Using Linked List
+#include<iostream>
+using namespace std;
+
+template <typename T>
+class Node{
+  T data;
+  Node<T> *next;
+  public:
+      Node(T data){
+       this->data = data;
+       next = NULL;
+      }
+} ;
+
+template<typename T>
+class Stack{
+   Node<T> *head;
+   int size;
+   public:
+       Stack(){
+          head = NULL;
+          size = 0;
+       }
+       int getSize(){
+       
+       }
+       bool isEmpty(){
+       
+       }
+       void push(T ele){
+       
+       }
+       void pop(){
+       
+       }
+       T top(){
+         
+       }
+};
+
+int main(){
+   Stack<int> s;
+   return 0;
+}
