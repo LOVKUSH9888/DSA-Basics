f8b9df7c56484f5909da95c12218b0487e9e45dd
LOVKUSH <lovkushyadav9888@gmail.com>
2023-04-12 22:11:44 +0530

    Problem-2

--- BINARY-SEARCH/5_EASY_PROBLEMS.cpp
+++ BINARY-SEARCH/5_EASY_PROBLEMS.cpp
@@ -34,3 +34,45 @@ int main() {
     cout << "Index of " << x << " is: " << index << endl;
     return 0;
 }
+
+
+///Problem 2: Given a sorted array of integers, find the index of the first occurrence of a particular element in the array.
+
+Example:
+Input:
+arr[] = {1, 2, 2, 3, 3, 3, 4, 4, 5}
+x = 3
+
+Output:
+3
+
+#include <iostream>
+using namespace std;
+
+int binarySearch(int arr[], int n, int x) {
+    int low = 0, high = n - 1;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        if (arr[mid] == x) {
+            if (mid == 0 || arr[mid - 1] != x) {
+                return mid;
+            } else {
+                high = mid - 1;
+            }
+        } else if (arr[mid] < x) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+    return -1;
+}
+
+int main() {
+    int arr[] = {1, 2, 2, 3, 3, 3, 4, 4, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int x = 3;
+    int index = binarySearch(arr, n, x);
+    cout << "Index of first occurrence of " << x << " is: " << index << endl;
+    return 0;
+}
---
+++ BINARY-SEARCH/tempCodeRunnerFile.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int binarySearch(int arr[], int n, int x) {
+    int low = 0, high = n - 1;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        if (arr[mid] == x) {
+            return mid;
+        } else if (arr[mid] < x) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+    return -1;
+}
+
+int main() {
+    int arr[] = {1, 3, 5, 7, 9};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int x = 5;
+    int index = binarySearch(arr, n, x);
+    cout << "Index of " << x << " is: " << index << endl;
+    return 0;
+}
