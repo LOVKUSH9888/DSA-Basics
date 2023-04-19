ec7216b6237ac445c9834d427f9d8dfd5edb0e37
LOVKUSH <lovkushyadav9888@gmail.com>
2023-04-19 21:46:45 +0530

    Oops-revision

---
+++ OOPS/Prepare.cpp
@@ -0,0 +1,6 @@
+//Why oop ?
+So before the OOP we have the procedure oriented programming
+-Which was conventional programming paradigm that uses the high level languages - c, cocol, fortran
+-Pragram divided into the various tasks - Many functions
+##Issues:-
+-Data was globally available so it was be access by the any of the function
